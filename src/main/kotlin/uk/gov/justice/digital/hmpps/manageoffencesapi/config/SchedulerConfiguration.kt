e34195b34fa33ac94fb310bd139e22b1a4c7e3d2
yasin <yasin.mustafa@gmail.com>
2022-07-18 15:40:50 +0100

    OFFENCES-95 prevent scheduler from running during test phase

--- src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/config/SchedulerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/config/SchedulerConfiguration.kt
@@ -5,10 +5,12 @@ import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.scheduling.annotation.EnableScheduling
 import javax.sql.DataSource
 
 @Configuration
+@Profile("!test") // prevent scheduler running during integration tests
 @EnableScheduling
 @EnableSchedulerLock(
   defaultLockAtLeastFor = "PT10M",
