08f6d68d41cb251b5806b97b0854577909c4484e
yasin <yasin.mustafa@gmail.com>
2022-12-14 19:43:13 +0000

    MOP-29 New repository method to lookup by cache

--- src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/repository/OffenceRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/repository/OffenceRepository.kt
@@ -4,19 +4,23 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.manageoffencesapi.entity.Offence
+import uk.gov.justice.digital.hmpps.manageoffencesapi.enum.SdrsCache
 import java.util.Optional
 
 @Repository
 interface OffenceRepository : JpaRepository<Offence, Long> {
   fun findByCodeStartsWithIgnoreCase(code: String): List<Offence>
+  fun findBySdrsCache(sdrsCache: SdrsCache): List<Offence>
   fun findOneByCode(code: String): Optional<Offence>
 
-  @Query("SELECT o FROM Offence o WHERE o.code LIKE :alphaChar% AND LENGTH(o.code) > 7 AND o.parentOffenceId IS NULL")
-  fun findChildOffencesWithNoParent(alphaChar: Char): List<Offence>
+  @Query("SELECT o FROM Offence o WHERE o.sdrsCache = :sdrsCache AND LENGTH(o.code) > 7 AND o.parentOffenceId IS NULL")
+  fun findChildOffencesWithNoParent(sdrsCache: SdrsCache): List<Offence>
 
   fun findByParentOffenceId(parentOffenceId: Long): List<Offence>
 
   fun findByParentOffenceIdIn(parentOffenceIds: Set<Long>): List<Offence>
 
   fun deleteByParentOffenceIdIsNotNull()
+
+  fun findByCodeIn(codes: Set<String>): List<Offence>
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/repository/SdrsLoadResultRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/repository/SdrsLoadResultRepository.kt
@@ -3,8 +3,9 @@ package uk.gov.justice.digital.hmpps.manageoffencesapi.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.manageoffencesapi.entity.SdrsLoadResult
+import uk.gov.justice.digital.hmpps.manageoffencesapi.enum.SdrsCache
 
 @Repository
-interface SdrsLoadResultRepository : JpaRepository<SdrsLoadResult, String> {
-  fun findAllByOrderByAlphaCharAsc(): List<SdrsLoadResult>
+interface SdrsLoadResultRepository : JpaRepository<SdrsLoadResult, SdrsCache> {
+  fun findAllByOrderByCacheAsc(): List<SdrsLoadResult>
 }
