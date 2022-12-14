859aab2325e957a63d66817b033b6f4e308c377c
yasin <yasin.mustafa@gmail.com>
2022-12-14 19:40:21 +0000

    MOP-29 Use SDRS Cache Enum and make fieelds mandatory that have been confirmed by SDRS

--- src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/entity/SdrsLoadResultHistory.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/entity/SdrsLoadResultHistory.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.manageoffencesapi.entity
 
 import uk.gov.justice.digital.hmpps.manageoffencesapi.enum.LoadStatus
 import uk.gov.justice.digital.hmpps.manageoffencesapi.enum.LoadType
+import uk.gov.justice.digital.hmpps.manageoffencesapi.enum.SdrsCache
 import java.time.LocalDateTime
 import javax.persistence.Entity
 import javax.persistence.EnumType
@@ -17,7 +18,8 @@ data class SdrsLoadResultHistory(
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   val id: Long = -1,
-  val alphaChar: String,
+  @Enumerated(EnumType.STRING)
+  val cache: SdrsCache,
   @Enumerated(EnumType.STRING)
   val status: LoadStatus? = null,
   @Enumerated(EnumType.STRING)
