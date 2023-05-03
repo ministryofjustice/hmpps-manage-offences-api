ffcf927f68ec9c467acb50c4f494f2975715961f
yasin <yasin.mustafa@gmail.com>
2023-05-03 11:49:56 +0100

    MOP-56 Add aws sts dependency to solve To use web identity tokens, the 'sts' service module must be on the class path.

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
 
   // AWS
   implementation("io.awspring.cloud:spring-cloud-aws-starter-s3")
+  implementation("com.amazonaws:aws-java-sdk-sts:1.12.460")
 
   // Test dependencies
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
