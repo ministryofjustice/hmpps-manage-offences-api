8ee79cf146afaa0787a8abec94feccdd4e6d711b
Yasin Mustafa <yasin.mustafa@digital.justice.gov.uk>
2023-05-03 13:56:49 +0100

    MOP-56 add additional endpoints to test aws s3 integration (#115)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/resource/TestController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/resource/TestController.kt
@@ -1,9 +1,11 @@
 package uk.gov.justice.digital.hmpps.manageoffencesapi.resource
 
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.manageoffencesapi.service.HoCodeService
@@ -13,13 +15,39 @@ import uk.gov.justice.digital.hmpps.manageoffencesapi.service.HoCodeService
 class TestController(
   private val hoCodeService: HoCodeService,
 ) {
-  @GetMapping(value = ["/s3-integration"])
+  @GetMapping(value = ["/s3-list-buckets"])
   @Operation(
-    summary = "Test AP S3 integration",
+    summary = "Test AP S3 integration - list buckets",
   )
-  fun testS3Integration(): String {
-    log.info("Request received to test s3 integration")
-    return hoCodeService.testS3Integration()
+  fun testS3ListBuckets(): String {
+    log.info("Request received to test s3 list buckets")
+    return hoCodeService.testS3ListBuckets()
+  }
+
+  @GetMapping(value = ["/s3-list-objects/{bucket}"])
+  @Operation(
+    summary = "Test AP S3 integration - list objects",
+  )
+  fun testS3ListObjects(
+    @Parameter(required = true, example = "bucket1", description = "The bucket name")
+    @PathVariable("bucket")
+    bucket: String,
+  ): String {
+    log.info("Request received to test s3 list objects")
+    return hoCodeService.testS3ListObjects(bucket)
+  }
+
+  @GetMapping(value = ["/s3-bucket-location/{bucket}"])
+  @Operation(
+    summary = "Test AP S3 integration - bucket location",
+  )
+  fun testS3GetBucketLocation(
+    @Parameter(required = true, example = "bucket1", description = "The bucket name")
+    @PathVariable("bucket")
+    bucket: String,
+  ): String {
+    log.info("Request received to test s3 list objects")
+    return hoCodeService.testS3GetBucketLocation(bucket)
   }
 
   companion object {
--- src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/service/HoCodeService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/service/HoCodeService.kt
@@ -4,27 +4,46 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.GetBucketLocationRequest
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Request
 
 @Service
 class HoCodeService(
   private val s3Client: S3Client,
 ) {
-  fun testS3Integration(): String {
-    log.info("Fetching from S3 start")
+  fun testS3ListBuckets(): String {
+    log.info("testS3ListBuckets start")
     val listOfbuckets = s3Client.listBuckets()
     log.info("listOfbuckets: $listOfbuckets")
+    return listOfbuckets.toString()
+  }
+  fun testS3ListObjects(bucket: String): String {
+    log.info("testS3ListObjects for $bucket start")
 
-    val listObjectsInFolder = s3Client.listObjectsV2(
+    val listObjectsInBucket = s3Client.listObjectsV2(
       ListObjectsV2Request
         .builder()
-        .bucket("mojap-manage-offences")
+        .bucket(bucket)
         .build(),
     )
 
-    log.info("List of objects in folder $listObjectsInFolder")
-    log.info("Fetching from S3 finish")
-    return listOfbuckets.toString()
+    log.info("List of objects in bucket $listObjectsInBucket")
+    log.info("testS3ListObjects finish")
+    return listObjectsInBucket.toString()
+  }
+
+  fun testS3GetBucketLocation(bucket: String): String {
+    log.info("testS3ListObjectsInBucket start for bucket $bucket")
+
+    val bucketLocation = s3Client.getBucketLocation(
+      GetBucketLocationRequest.builder()
+        .bucket(bucket)
+        .build(),
+    )
+
+    log.info("Bucket location $bucketLocation")
+    log.info("testS3GetBucketLocation finish")
+    return bucketLocation.toString()
   }
 
   companion object {
