c9a7d1f35fae8cf0793446b223a2404d770ee3fa
Yasin Mustafa <yasin.mustafa@digital.justice.gov.uk>
2023-05-04 09:28:47 +0100

    MOP-56 test endpoint doing a select query of the parquet files in the AP s3 bucket (#116)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/config/AwsConfiguration.kt
@@ -0,0 +1,36 @@
+package uk.gov.justice.digital.hmpps.manageoffencesapi.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
+import software.amazon.awssdk.http.async.SdkAsyncHttpClient
+import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.S3AsyncClient
+import software.amazon.awssdk.services.s3.S3Configuration
+import java.time.Duration
+
+@Configuration
+class AwsConfiguration(
+  @Value("\${aws.region}") private val awsRegion: String,
+) {
+  @Bean
+  fun s3AsyncClient(
+    credentialsProvider: AwsCredentialsProvider,
+  ): S3AsyncClient {
+    val httpClient: SdkAsyncHttpClient = NettyNioAsyncHttpClient.builder()
+      .writeTimeout(Duration.ZERO)
+      .maxConcurrency(64)
+      .build()
+    val serviceConfiguration = S3Configuration.builder()
+      .checksumValidationEnabled(false)
+      .chunkedEncodingEnabled(true)
+      .build()
+    return S3AsyncClient.builder().httpClient(httpClient)
+      .region(Region.of(awsRegion))
+      .credentialsProvider(credentialsProvider)
+      .serviceConfiguration(serviceConfiguration)
+      .build()
+  }
+}
