b60c9883bbce5e3a32747152a7ab4e441ffd58ef
Joel Stobart (MOJ) <87521440+joelstobart-moj@users.noreply.github.com>
2023-06-12 14:01:44 +0100

    Fix to versions (#139)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.1.4"
-  kotlin("plugin.spring") version "1.8.21"
-  kotlin("plugin.jpa") version "1.8.21"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.2.0"
+  kotlin("plugin.spring") version "1.8.22"
+  kotlin("plugin.jpa") version "1.8.22"
 }
 
 configurations {
@@ -19,10 +19,10 @@ dependencies {
 
   // Database dependencies
   runtimeOnly("org.flywaydb:flyway-core")
-  runtimeOnly("org.postgresql:postgresql:42.5.4")
+  runtimeOnly("org.postgresql:postgresql:42.6.0")
 
   // AppInsights
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.23.0")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.26.0")
 
   // JWT
   implementation("io.jsonwebtoken:jjwt-api:0.11.5")
@@ -40,28 +40,28 @@ dependencies {
   // OpenAPI
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
 
-  implementation("io.hypersistence:hypersistence-utils-hibernate-60:3.3.2")
+  implementation("io.hypersistence:hypersistence-utils-hibernate-60:3.4.2")
 
   // Schedule locking
-  implementation("net.javacrumbs.shedlock:shedlock-spring:5.1.0")
-  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:5.1.0")
+  implementation("net.javacrumbs.shedlock:shedlock-spring:5.4.0")
+  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:5.4.0")
 
   // SQS
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:2.0.0")
 
   // AWS
   implementation("io.awspring.cloud:spring-cloud-aws-starter-s3")
-  implementation("software.amazon.awssdk:sts:2.20.65")
-  implementation("software.amazon.awssdk:netty-nio-client:2.20.65")
+  implementation("software.amazon.awssdk:sts:2.20.83")
+  implementation("software.amazon.awssdk:netty-nio-client:2.20.83")
 
   // Miscellaneous
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.0")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
 
   // Test dependencies
-  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.1.3")
+  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.1.15")
   testImplementation("com.h2database:h2")
   testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:2.35.0")
-  testImplementation("io.opentelemetry:opentelemetry-sdk-testing:1.23.1")
+  testImplementation("io.opentelemetry:opentelemetry-sdk-testing:1.27.0")
 }
 
 java {
