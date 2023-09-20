cb4de4de139e28d255e1be063af19ad139998e5f
yasin <yasin.mustafa@gmail.com>
2023-09-20 10:21:54 +0100

    MOP-105 temporarily revert previous commit (the spring upgrade conflicts with the aws libraries) - will revisit after the next feature change

--- src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/config/ResourceServerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/config/ResourceServerConfiguration.kt
@@ -2,15 +2,11 @@ package uk.gov.justice.digital.hmpps.manageoffencesapi.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @Configuration
 @EnableWebSecurity
@@ -18,35 +14,26 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 class ResourceServerConfiguration {
 
   @Bean
-  fun web(http: HttpSecurity): SecurityFilterChain {
-    http {
-      sessionManagement {
-        sessionCreationPolicy = SessionCreationPolicy.STATELESS
+  fun filterChain(http: HttpSecurity): SecurityFilterChain {
+    val chain = http
+      .sessionManagement()
+      .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+      .and().headers().frameOptions().sameOrigin()
+      .and().csrf().disable()
+      .authorizeHttpRequests { auth ->
+        auth.requestMatchers(
+          "/webjars/**",
+          "favicon.ico",
+          "/health/**",
+          "/info",
+          "/swagger-resources/**",
+          "/v3/api-docs/**",
+          "/swagger-ui/**",
+          "/swagger-ui.html",
+          "/h2-console/**",
+        ).permitAll().anyRequest().authenticated()
       }
-      csrf { disable() }
-      headers {
-        referrerPolicy {
-          policy = ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN
-        }
-      }
-      authorizeHttpRequests {
-        authorize(AntPathRequestMatcher("/webjars/**", HttpMethod.GET.name()), permitAll)
-        authorize(AntPathRequestMatcher("favicon.ico", HttpMethod.GET.name()), permitAll)
-        authorize(AntPathRequestMatcher("/health/**", HttpMethod.GET.name()), permitAll)
-        authorize(AntPathRequestMatcher("/info", HttpMethod.GET.name()), permitAll)
-        authorize(AntPathRequestMatcher("/swagger-resources/**", HttpMethod.GET.name()), permitAll)
-        authorize(AntPathRequestMatcher("/v3/api-docs/**", HttpMethod.GET.name()), permitAll)
-        authorize(AntPathRequestMatcher("/swagger-ui/**", HttpMethod.GET.name()), permitAll)
-        authorize(AntPathRequestMatcher("/swagger-ui.html", HttpMethod.GET.name()), permitAll)
-        authorize(AntPathRequestMatcher("/h2-console/**", HttpMethod.POST.name()), permitAll)
-        authorize(anyRequest, authenticated)
-      }
-      oauth2ResourceServer {
-        jwt {
-          jwtAuthenticationConverter = AuthAwareTokenConverter()
-        }
-      }
-    }
-    return http.build()
+    chain.oauth2ResourceServer().jwt().jwtAuthenticationConverter(AuthAwareTokenConverter())
+    return chain.build()
   }
 }
