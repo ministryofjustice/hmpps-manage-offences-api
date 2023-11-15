d93714942dcd6aac7a82414efc881351d80f8fd1
yasin <yasin.mustafa@gmail.com>
2023-11-15 11:11:56 +0000

    MOP-122 Add comments and correct flyway script

--- src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/enum/NomisScheduleName.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/enum/NomisScheduleName.kt
@@ -1,10 +1,10 @@
 package uk.gov.justice.digital.hmpps.manageoffencesapi.enum
 
 enum class NomisScheduleName {
-  PCSC_SDS, // SDS between 4 and 7 years = List B: Schedule 15 Part 2 that attract life + serious violent offences (same as List C)
+  PCSC_SDS, // List B: SDS between 4 and 7 years : Schedule 15 Part 2 that attract life + serious violent offences (same as List C)
   PCSC_SDS_PLUS, // List D: Schedule 15 Part 1 + Schedule 15 Part 2 that attract life
-  PCSC_SEC_250, // Sec250 >7 years = List C: Schedule 15 Part 2 that attract life + serious violent offences (same as List B)
+  PCSC_SEC_250, // List C: Sec250 >7 years = List C: Schedule 15 Part 2 that attract life + serious violent offences (same as List B)
   SCHEDULE_15,
-  SCHEDULE_15_ATTRACTS_LIFE,
+  SCHEDULE_15_ATTRACTS_LIFE, // List A - Schedule 15 Part 1 + Schedule 15 Part 2 that attract life (exclude all offences that start after 28 June 2022)
   POTENTIAL_LINK_PCSC, // Used for potential pcsc mappings
 }
--- src/main/resources/migration/common/V42__add_schedule_nomis_columns.sql
+++ src/main/resources/migration/common/V42__add_schedule_nomis_columns.sql
@@ -27,7 +27,7 @@ INSERT INTO offence_to_sync_with_nomis
      )
      order by o.code );
 
--- SCHEDULE_15 link to nomis where in s15 part 1 and 2 but not already linked (could already be linked by the legacy mappings)
+-- SCHEDULE_15 link to nomis where in s15 part 1 but not already linked (could already be linked by the legacy mappings)
 INSERT INTO offence_to_sync_with_nomis
 (offence_code, nomis_sync_type, nomis_schedule_name, created_date)
     (select o2.code, 'LINK_SCHEDULE_TO_OFFENCE', 'SCHEDULE_15', NOW()
@@ -47,7 +47,7 @@ INSERT INTO offence_to_sync_with_nomis
                              and sp.part_number in (4, 5,6))
      order by o2.code );
 
--- SCHEDULE_15 link to nomis where in s15 part 1 and 2 but not already linked (could already be linked by the legacy mappings)
+-- SCHEDULE_15 link to nomis where in s15 part 2 but not already linked (could already be linked by the legacy mappings)
 INSERT INTO offence_to_sync_with_nomis
 (offence_code, nomis_sync_type, nomis_schedule_name, created_date)
     (select o2.code, 'LINK_SCHEDULE_TO_OFFENCE', 'SCHEDULE_15', NOW()
@@ -74,65 +74,13 @@ INSERT INTO offence_to_sync_with_nomis
         )
      order by o2.code );
 
--- SCHEDULE_15_ATTRACTS_LIFE unlink from nomis where not in s15-life-nomis FROM HMCTS
-INSERT INTO offence_to_sync_with_nomis
-(offence_code, nomis_sync_type, nomis_schedule_name, created_date)
-    (select o.code, 'UNLINK_SCHEDULE_TO_OFFENCE', 'SCHEDULE_15_ATTRACTS_LIFE', NOW()
-     from nomis_schedule_mapping nsm
-              join schedule_part sp on sp.id = nsm.schedule_part_id
-              join schedule s on s.id = sp.schedule_id
-              join offence_schedule_mapping osm on osm.schedule_part_id = sp.id
-              join offence o on osm.offence_id = o.id
-     where s.code = 'PCSC and Legacy NOMIS'
-       and sp.part_number in (7)
-       and o.code not in (
-         select o2.code
-         from offence_schedule_mapping osm15
-                  join schedule_part sp15 on sp15.id = osm15.schedule_part_id
-                  join schedule s15 on sp15.schedule_id = s15.id
-                  join offence o2 on osm15.offence_id = o2.id
-         where s15.code = '15'
-           and sp15.part_number in (1,2, 3)
-           and o2.max_period_is_life = true
-     )
-     order by o.code );
-
--- SCHEDULE_15_ATTRACTS_LIFE link to nomis where in s15 part 1 and 2 and attracts life but not already linked
-INSERT INTO offence_to_sync_with_nomis
-(offence_code, nomis_sync_type, nomis_schedule_name, created_date)
-    (select o2.code, 'LINK_SCHEDULE_TO_OFFENCE', 'SCHEDULE_15', NOW()
-     from offence_schedule_mapping osm15
-              join schedule_part sp15 on sp15.id = osm15.schedule_part_id
-              join schedule s15 on sp15.schedule_id = s15.id
-              join offence o2 on osm15.offence_id = o2.id
-     where s15.code = '15'
-       and sp15.part_number in (1,2)
-       and o2.max_period_is_life = true
-       and o2.code not in (select o.code
-                           from nomis_schedule_mapping nsm
-                                    join schedule_part sp on sp.id = nsm.schedule_part_id
-                                    join schedule s on s.id = sp.schedule_id
-                                    join offence_schedule_mapping osm on osm.schedule_part_id = sp.id
-                                    join offence o on osm.offence_id = o.id
-                           where s.code = 'PCSC and Legacy NOMIS'
-                             and sp.part_number in (7))
-      and not exists (
-          SELECT NULL
-          FROM offence_to_sync_with_nomis  otsn
-          WHERE otsn.nomis_sync_type = 'LINK_SCHEDULE_TO_OFFENCE'
-          AND otsn.nomis_schedule_name = 'SCHEDULE_15'
-          AND otsn.offence_code = o2.code
-     )
-     order by o2.code );
-
-
-
--- PCSC_SDS, PCSC_SDS_PLUS, PCSC_SEC_250 unlink all from NOMIS
+-- PCSC_SDS, PCSC_SDS_PLUS, PCSC_SEC_250, SCHEDULE_15_ATTRACTS_LIFE unlink all from NOMIS
 INSERT INTO offence_to_sync_with_nomis
 (offence_code, nomis_sync_type, nomis_schedule_name, created_date)
     (select o.code, 'UNLINK_SCHEDULE_TO_OFFENCE', CASE WHEN sp.part_number = 1 THEN 'PCSC_SDS'
                                                                WHEN sp.part_number = 2 THEN 'PCSC_SDS_PLUS'
                                                                WHEN sp.part_number = 3 THEN 'PCSC_SEC_250'
+                                                               WHEN sp.part_number = 7 THEN 'SCHEDULE_15_ATTRACTS_LIFE'
                                                             END
         , NOW()
      from nomis_schedule_mapping nsm
@@ -141,7 +89,7 @@ INSERT INTO offence_to_sync_with_nomis
               join offence_schedule_mapping osm on osm.schedule_part_id = sp.id
               join offence o on osm.offence_id = o.id
      where s.code = 'PCSC and Legacy NOMIS'
-       and sp.part_number in (1,2,3)
+       and sp.part_number in (1,2,3,7)
      order by o.code );
 
 -- schedule 15 potentially link all from parts 1 and 2 to NOMIS PCSC indicators (actual pcsc check done in code)
