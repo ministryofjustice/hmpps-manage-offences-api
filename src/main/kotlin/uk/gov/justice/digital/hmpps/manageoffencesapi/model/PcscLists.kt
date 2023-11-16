59a5ab3b1df5b27799712df45563ca48b61855e9
yasin <yasin.mustafa@gmail.com>
2023-11-16 22:20:07 +0000

    MOP-125 Add endpoint to fetch all PCSC offences organised per list

--- src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/model/PcscLists.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/model/PcscLists.kt
@@ -1,8 +1,14 @@
 package uk.gov.justice.digital.hmpps.manageoffencesapi.model
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class PcscLists(
+  @Schema(description = "Schedule 15 Part 1 + Schedule 15 Part 2 that attract life (exclude all offences that start on or after 28 June 2022)")
   val listA: Set<OffenceToScheduleMapping> = emptySet(),
+  @Schema(description = "SDS between 4 and 7 years : Schedule 15 Part 2 that attract life + serious violent offences")
   val listB: Set<OffenceToScheduleMapping> = emptySet(),
+  @Schema(description = "Sec250 >7 years = List C: Schedule 15 Part 2 that attract life + serious violent offences (same as List B)")
   val listC: Set<OffenceToScheduleMapping> = emptySet(),
+  @Schema(description = "Schedule 15 Part 1 + Schedule 15 Part 2 that attract life")
   val listD: Set<OffenceToScheduleMapping> = emptySet(),
 )
--- src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/model/PcscMarkers.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/model/PcscMarkers.kt
@@ -1,12 +1,19 @@
 package uk.gov.justice.digital.hmpps.manageoffencesapi.model
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class PcscMarkers(
+  @Schema(description = "Schedule 15 Part 1 + Schedule 15 Part 2 that attract life (exclude all offences that start on or after 28 June 2022)")
   val inListA: Boolean,
+  @Schema(description = "SDS between 4 and 7 years : Schedule 15 Part 2 that attract life + serious violent offences")
   val inListB: Boolean,
+  @Schema(description = "Sec250 >7 years = List C: Schedule 15 Part 2 that attract life + serious violent offences (same as List B)")
   val inListC: Boolean,
+  @Schema(description = "Schedule 15 Part 1 + Schedule 15 Part 2 that attract life")
   val inListD: Boolean,
 )
 
+@Schema(description = "Shows which (if any) PCSC Marker the offence relates to")
 data class OffencePcscMarkers(
   val offenceCode: String,
   val pcscMarkers: PcscMarkers,
