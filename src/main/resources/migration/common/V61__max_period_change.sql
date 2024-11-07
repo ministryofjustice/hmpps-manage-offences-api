a5e45b26d8e65a79db2eb7a6629944f8e6745317
yasin <yasin.mustafa@gmail.com>
2024-11-07 11:13:21 +0000

    MOP-196 RT88501 should have max period of 14 years

---
+++ src/main/resources/migration/common/V61__max_period_change.sql
@@ -0,0 +1,4 @@
+UPDATE offence SET max_period_is_life = FALSE WHERE code = 'RT88501';
+UPDATE offence SET max_period_is_life = FALSE WHERE code = 'RT88501E';
+UPDATE offence SET max_period_of_indictment_years = 14 WHERE code = 'RT88501';
+UPDATE offence SET max_period_of_indictment_years = 14 WHERE code = 'RT88501E';
