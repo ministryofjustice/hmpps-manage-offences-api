499aee4fab545a504d59e0a555ec9cf1c04f1cd7
matthew.green <matthew.green3@digital.justice.gov.uk>
2024-12-04 15:57:36 +0000

    CRS-2209-tranche-three-refactor
    Update migration to include Murder offences under new Murder Schedule.
    
    Linking the offences manually will include encouragement offences we don't want for SDS exclusions.

--- src/main/resources/migration/common/V63__T3_schedule_setup.sql
+++ src/main/resources/migration/common/V63__T3_schedule_setup.sql
@@ -1,7 +1,12 @@
-INSERT INTO schedule (act, code, url) VALUES
-       ('Tranche Three Murder Excluded Offences', 'MURD', null);
+INSERT INTO schedule (act, code, url)
+VALUES ('Tranche Three Murder Excluded Offences', 'MURD', null);
 
 INSERT INTO schedule_part (schedule_id, part_number)
 VALUES ((SELECT id FROM schedule WHERE code = 'SEO'), 2),
        ((SELECT id FROM schedule WHERE code = 'DVEO'), 2),
        ((SELECT id FROM schedule WHERE code = 'MURD'), 1);
+
+INSERT INTO offence_schedule_mapping (schedule_part_id, legislation_text, offence_id)
+VALUES
+    ((SELECT id FROM schedule_part WHERE schedule_id = (SELECT id FROM schedule WHERE code = 'MURD') AND part_number = 1), 'Contrary to Common Law', (SELECT id FROM offence WHERE code = 'COML025')),
+    ((SELECT id FROM schedule_part WHERE schedule_id = (SELECT id FROM schedule WHERE code = 'MURD') AND part_number = 1), 'Contrary to Common Law', (SELECT id FROM offence WHERE code = 'COML026'));
