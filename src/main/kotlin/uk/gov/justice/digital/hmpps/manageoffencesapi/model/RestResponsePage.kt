f3f951399bf96ac716219dcc46e8a29cfbdf58f5
Yasin Mustafa <yasin.mustafa@gmail.com>
2026-03-13 16:45:23 +0000

    switch to data class

--- src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/model/RestResponsePage.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/manageoffencesapi/model/RestResponsePage.kt
@@ -2,31 +2,17 @@
 
 package uk.gov.justice.digital.hmpps.manageoffencesapi.model
 
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
 import tools.jackson.databind.JsonNode
-import org.springframework.data.domain.PageImpl
-import org.springframework.data.domain.PageRequest
-import org.springframework.data.domain.Pageable
 
-class RestResponsePage<T : Any> : PageImpl<T> {
-  @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
-  constructor(
-    @JsonProperty("content") content: List<T>,
-    @JsonProperty("number") number: Int,
-    @JsonProperty("size") size: Int,
-    @JsonProperty("totalElements") totalElements: Long?,
-    @JsonProperty("pageable") pageable: JsonNode,
-    @JsonProperty("last") last: Boolean,
-    @JsonProperty("totalPages") totalPages: Int,
-    @JsonProperty("sort") sort: JsonNode,
-    @JsonProperty("first") first: Boolean,
-    @JsonProperty("numberOfElements") numberOfElements: Int,
-  ) : super(content, PageRequest.of(number, size), totalElements ?: 0L)
-
-  constructor(content: List<T>, pageable: Pageable, total: Long) : super(content, pageable, total)
-
-  constructor(content: List<T>) : super(content)
-
-  constructor() : super(emptyList<T>())
-}
+data class RestResponsePage<T>(
+  val content: List<T>,
+  val number: Int,
+  val size: Int,
+  val totalElements: Long?,
+  val pageable: JsonNode,
+  val last: Boolean,
+  val totalPages: Int,
+  val sort: JsonNode,
+  val first: Boolean,
+  val numberOfElements: Int,
+)
